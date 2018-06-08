3766967367ac3cd44ba7efba4de396e1a01b1400
Jason E Bailey <jeb@apache.org>
2018-06-08 14:53:24 -0400

    updated test package

--- resource-predicates/src/test/java/org/apache/sling/resource/predicates/ResourcePredicateTest.java
+++ resource-predicates/src/test/java/org/apache/sling/resource/predicates/ResourcePredicateTest.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.sling.resource.stream;
+package org.apache.sling.resource.predicates;
 
 import static org.apache.sling.resource.predicates.ChildResourcePredicates.child;
 import static org.apache.sling.resource.predicates.PropertyPredicates.property;
