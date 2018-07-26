4288be69c9c7b4ec677bccc6284e3f0ad6d4b190
Bertrand Delacretaz <bdelacretaz@apache.org>
2018-07-26 16:17:28 +0200

    Use Karate naming conventions

---
+++ literate-http-testing/karate/src/test/java/sling/SlingTest.java
@@ -0,0 +1,9 @@
+package sling;
+
+import com.intuit.karate.junit4.Karate;
+import org.junit.runner.RunWith;
+
+// Run all *.feature files found in and under this folder
+@RunWith(Karate.class)
+public class SlingTest {
+}
