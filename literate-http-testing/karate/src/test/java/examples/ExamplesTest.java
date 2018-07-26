63c34606841a1bb1fb7c6f1f2c92037f8551700a
Bertrand Delacretaz <bdelacretaz@apache.org>
2018-07-26 16:05:50 +0200

    Karate skeleton, from mvn archetype

---
+++ literate-http-testing/karate/src/test/java/examples/ExamplesTest.java
@@ -0,0 +1,11 @@
+package examples;
+
+import com.intuit.karate.junit4.Karate;
+import org.junit.runner.RunWith;
+
+@RunWith(Karate.class)
+public class ExamplesTest {
+    // this will run all *.feature files that exist in sub-directories
+    // refer to https://github.com/intuit/karate#naming-conventions
+    // for folder-structure recommendations and naming conventions
+}
