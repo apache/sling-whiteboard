0df460e5296fbb522e3a05c8217d8592d618b4c5
Bertrand Delacretaz <bdelacretaz@apache.org>
2018-09-06 10:47:11 +0200

    Fix Gatling test

--- karate-http-testing/src/test/scala/sling/PostServletSimulation.scala
+++ karate-http-testing/src/test/scala/sling/PostServletSimulation.scala
@@ -19,8 +19,14 @@ class PostServletSimulation extends Simulation {
     "/importContentTest/*" -> pauseFor("post" -> 7, "get" -> 15, "delete" -> 50),
   )
 
-  val createContent = scenario("create").exec(karateFeature("classpath:sling/postservlet/createcontent/createContent.feature"))
-  val importContent = scenario("import").exec(karateFeature("classpath:sling/postservlet/createcontent/importContent.feature"))
+  val createContent = scenario("create")
+    .exec(
+      karateFeature("classpath:sling/postservlet/createContent.feature")
+    )
+  val importContent = scenario("import")
+    .exec(
+      karateFeature("classpath:sling/postservlet/importContent.feature")
+    )
 
   setUp(
     createContent.inject(rampUsers(75) over (5 seconds)).protocols(protocol),
