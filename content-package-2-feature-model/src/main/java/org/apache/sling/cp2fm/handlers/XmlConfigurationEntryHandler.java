80bee53b2850ee0eb8b430b6ffc957ed57c201ee
Simo Tripodi <stripodi@adobe.com>
2019-03-04 23:50:43 +0100

    [cp2fm] reduced XML parsing files names inclusion

--- content-package-2-feature-model/src/main/java/org/apache/sling/cp2fm/handlers/XmlConfigurationEntryHandler.java
+++ content-package-2-feature-model/src/main/java/org/apache/sling/cp2fm/handlers/XmlConfigurationEntryHandler.java
@@ -39,7 +39,7 @@ public final class XmlConfigurationEntryHandler extends AbstractSingleConfigurat
     private final SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
 
     public XmlConfigurationEntryHandler() {
-        super("[^.][^/]+\\.xml");
+        super("[^._][^/]+\\.xml");
     }
 
     @Override
