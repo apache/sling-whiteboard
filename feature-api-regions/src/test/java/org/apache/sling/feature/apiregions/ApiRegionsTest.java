242e99cdf2ab342952c503edc517832d57a82fc0
Simo Tripodi <stripodi@adobe.com>
2019-04-01 15:34:26 +0200

    [api-regions] initial checkin of api-regions APIs proposal

---
+++ feature-api-regions/src/test/java/org/apache/sling/feature/apiregions/ApiRegionsTest.java
@@ -0,0 +1,106 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.apache.sling.feature.apiregions;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ApiRegionsTest {
+
+    private ApiRegions apiRegions;
+
+    @Before
+    public void setUp() {
+        apiRegions = new ApiRegions();
+    }
+
+    @After
+    public void tearDown() {
+        apiRegions = null;
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void canNotCreateRegionWithNullName() {
+        apiRegions.addNewRegion(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void canNotCreateRegionWithEmptyName() {
+        apiRegions.addNewRegion("");
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void impossibleToGetRegionWithNullName() {
+        apiRegions.getByName(null);
+    }
+
+    @Test
+    public void impossibleToGetRegionWithEmptyName() {
+        assertNull(apiRegions.getByName(""));
+    }
+
+    @Test
+    public void impossibleToGetRegionWithNotRegisteredRegion() {
+        assertNull(apiRegions.getByName("not-registered-yet"));
+    }
+
+    @Test
+    public void getRegionWithRegisteredRegion() {
+        String regionName = "registered";
+        ApiRegion region = apiRegions.addNewRegion(regionName);
+        assertNotNull(region);
+        assertNotNull(apiRegions.getByName(regionName));
+    }
+
+    @Test
+    public void sequentialInheritance() {
+        ApiRegion granpa = apiRegions.addNewRegion("granpa");
+        ApiRegion father = apiRegions.addNewRegion("father");
+        ApiRegion child = apiRegions.addNewRegion("child");
+
+        assertSame(father, child.getParent());
+        assertSame(granpa, father.getParent());
+        assertNull(granpa.getParent());
+    }
+
+    @Test
+    public void iteratingOverRegions() {
+        apiRegions.addNewRegion("granpa");
+        apiRegions.addNewRegion("father");
+        apiRegions.addNewRegion("child");
+
+        List<String> expected = Arrays.asList("granpa", "father", "child");
+
+        List<String> actual = new LinkedList<>();
+        for (ApiRegion region : apiRegions) {
+            actual.add(region.getName());
+        }
+
+        assertEquals(expected, actual);
+    }
+
+}
---
+++ feature-api-regions/src/test/java/org/apache/sling/feature/apiregions/io/json/ApiRegionsJSONParserTest.java
@@ -0,0 +1,101 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.apache.sling.feature.apiregions.io.json;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import org.apache.sling.feature.ArtifactId;
+import org.apache.sling.feature.Extension;
+import org.apache.sling.feature.ExtensionType;
+import org.apache.sling.feature.Feature;
+import org.apache.sling.feature.apiregions.ApiRegion;
+import org.apache.sling.feature.apiregions.ApiRegions;
+import org.junit.Test;
+
+public class ApiRegionsJSONParserTest {
+
+    @Test(expected = NullPointerException.class)
+    public void canNotParseNullFeature() {
+        ApiRegionsJSONParser.parseApiRegions((Feature) null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void canNotParseNullExtension() {
+        ApiRegionsJSONParser.parseApiRegions((Extension) null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void canNotParseInvalidExtensionName() {
+        Extension extension = new Extension(ExtensionType.JSON, "invalid", false);
+        ApiRegionsJSONParser.parseApiRegions(extension);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void canNotParseInvalidExtensionType() {
+        Extension extension = new Extension(ExtensionType.TEXT, "api-regions", false);
+        ApiRegionsJSONParser.parseApiRegions(extension);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void canNotParseNullJsonRepresentation() {
+        ApiRegionsJSONParser.parseApiRegions((String) null);
+    }
+
+    @Test
+    public void parseApiRegions() {
+        Extension extension = new Extension(ExtensionType.JSON, "api-regions", false);
+        extension.setJSON("[\n" + 
+                            "  {\n" + 
+                            "    \"name\":\"base\",\n" + 
+                            "    \"exports\":[\n" + 
+                            "      \"org.apache.felix.metatype\",\n" + 
+                            "      \"org.apache.felix.inventory\"\n" + 
+                            "    ]\n" + 
+                            "  },\n" + 
+                            "  {\n" + 
+                            "    \"name\":\"extended\",\n" + 
+                            "    \"exports\":[\n" + 
+                            "      \"org.apache.felix.scr.component\",\n" + 
+                            "      \"org.apache.felix.scr.info\",\n" + 
+                            "      \"org.apache.felix.metatype\",\n" + 
+                            "      \"org.apache.felix.inventory\"\n" + 
+                            "    ]\n" + 
+                            "  }\n" + 
+                            "]");
+        Feature feature = new Feature(ArtifactId.fromMvnId("org.apache.sling:org.apache.sling.feature.apiregions:1.0.0"));
+        feature.getExtensions().add(extension);
+
+        ApiRegions apiRegions = ApiRegionsJSONParser.parseApiRegions(feature);
+
+        ApiRegion base = apiRegions.getByName("base");
+        assertNull(base.getParent());
+        assertTrue(base.contains("org.apache.felix.inventory"));
+        assertTrue(base.contains("org.apache.felix.metatype"));
+
+        ApiRegion extended = apiRegions.getByName("extended");
+        assertNotNull(extended.getParent());
+        assertSame(base, extended.getParent());
+        assertTrue(extended.contains("org.apache.felix.inventory"));
+        assertTrue(extended.contains("org.apache.felix.metatype"));
+        assertTrue(extended.contains("org.apache.felix.scr.component"));
+        assertTrue(extended.contains("org.apache.felix.scr.info"));
+    }
+
+}
