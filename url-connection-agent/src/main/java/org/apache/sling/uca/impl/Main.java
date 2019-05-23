7da88ff098521199065c765236e4442b60b84798
Robert Munteanu <rombert@apache.org>
2019-05-23 17:42:48 +0200

    POC for a Java Agent to set URL connection timeout defaults
    Completed initial implementation

--- url-connection-agent/src/main/java/org/apache/sling/uca/impl/Main.java
+++ url-connection-agent/src/main/java/org/apache/sling/uca/impl/Main.java
@@ -1,14 +1,31 @@
 package org.apache.sling.uca.impl;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class Main {
-    
+
     public static void main(String[] args) throws MalformedURLException, IOException {
         
-        new URL("http://sling.apache.org").openConnection();
+        if ( args.length != 1 )
+            throw new IllegalArgumentException("Usage: java -jar ... <URL>");
+
+        URLConnection con = new URL(args[0]).openConnection();
+        System.out.println("Connection type is " + con);
+        
+        try (InputStream in = con.getInputStream();
+                InputStreamReader isr = new InputStreamReader(in);
+                BufferedReader br = new BufferedReader(isr)) {
+            String line;
+            while ( (line = br.readLine()) != null )
+                System.out.println("[WEB] " + line);
+        }
+
     }
 
 }
