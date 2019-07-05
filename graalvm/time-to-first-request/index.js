2d4b7ff3936f89483d3234fb0efc3eb1e1ca8794
Bertrand Delacretaz <bdelacretaz@apache.org>
2019-07-05 16:58:37 +0200

    Switch to dockerode

--- graalvm/time-to-first-request/index.js
+++ graalvm/time-to-first-request/index.js
@@ -16,14 +16,22 @@
 //
 const http = require('http');
 const httpProxy = require('http-proxy');
-const { Docker } = require('docker-cli-js');
+const Docker = require('dockerode');
 const waitOn = require('wait-on');
 const elapsedTime = require('elapsed-time')
 
 const listenPort = 9000;
 const targetUrl = 'http://127.0.0.1:8080';
 const waitUrl = `${targetUrl}/sling/chouc/route`;
-const dockerRunCmd = 'run -i --rm -p 8080:8080 quarkus/org.apache.sling.graalvm.experiments';
+const dockerImage = 'quarkus/org.apache.sling.graalvm.experiments';
+const dockerStartOptions = {
+  PortBindings: {
+    "8080/tcp": [{
+        "HostIP":"0.0.0.0",
+        "HostPort": "8080"
+    }],
+  },
+};
 
 const proxy = httpProxy.createProxyServer({});
 
@@ -37,12 +45,15 @@ const waitOpts = {
     window: 1,
 };
 
+const docker = new Docker();
+
 var server = http.createServer(function(req, res) {
-  console.log(`Starting container: docker ${dockerRunCmd}`);
   const et = elapsedTime.new().start();
   // TODO detect if container is already running, instead of blind catch
   // (maybe use node-docker-api instead)
-  new Docker({}).command(dockerRunCmd).catch();
+
+  console.log(`Starting container ${dockerImage}(${et.getValue()})`);
+  docker.run(dockerImage, null, process.stderr, dockerStartOptions);
   console.log(`Waiting on ${waitOpts.resources[0]} (${et.getValue()})`);
   waitOn(waitOpts).then(() =>{
     console.log(`Time to first request to ${waitUrl}: (${et.getValue()})`);
