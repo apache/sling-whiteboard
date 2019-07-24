7a12dfebbbe01a8aeedc3a32ff37bbb33f55a3af
Bertrand Delacretaz <bdelacretaz@apache.org>
2019-07-24 14:05:34 +0200

    Improve install script

--- serverless-microsling/microsling.js
+++ serverless-microsling/microsling.js
@@ -1,7 +1,19 @@
 function main (params) {
+  const result = {
+    body: `${params.__ow_path}`,
+  };
+
   return new Promise(function (resolve, reject) {
-    return resolve({ body:"Ce texte-ci\n"});
+    return resolve(result);
   })
+}
+
+if (require.main === module) {
+  const result = main({
+    __ow_path: process.argv[2],
+    __ow_method: 'get',
+  });
+  console.log(result);
 }
 
 module.exports.main = main
