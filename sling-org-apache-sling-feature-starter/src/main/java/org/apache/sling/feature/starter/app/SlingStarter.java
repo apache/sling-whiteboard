4a903260e56a66568cbeec739c85c91a4c9026b2
Andreas Schaefer <schaefa@iMac.local>
2019-12-30 15:53:22 -0800

    Renamed to Sling Feature Starter and used picocli to handle parameters
    Also removed any usaged of the Launchpad Base

---
+++ sling-org-apache-sling-feature-starter/src/main/java/org/apache/sling/feature/starter/app/SlingStarter.java
@@ -0,0 +1,225 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.sling.feature.starter.app;
+
+import java.io.File;
+import java.io.PrintStream;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.sling.feature.launcher.impl.Main;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+
+@Command(
+    name = "sfl",
+    description = "Apache Sling Feature Starter",
+    footer = "Copyright(c) 2019 The Apache Software Foundation."
+)
+public class SlingStarter implements Runnable {
+
+    @Option(names = { "-s", "--mainFeature" }, description = "main feature file (file path or URL) replacing the provided Sling Feature File", required = false)
+    private String mainFeatureFile;
+
+    @Option(names = { "-af", "--additionalFeature" }, description = "additional feature files", required = false)
+    private List<String> additionalFeatureFile;
+
+    @Option(names = { "-j", "--control" }, description = "host and port to use for control connection in the format '[host:]port' (default 127.0.0.1:0)", required = false)
+    private String controlAddress;
+
+    @Option(names = { "-l", "--logLevel" }, description = "the initial loglevel (0..4, FATAL, ERROR, WARN, INFO, DEBUG)", required = false)
+    private String logLevel;
+
+    @Option(names = { "-f", "--logFile" }, description = "the log file, \"-\" for stdout (default logs/error.log)", required = false)
+    private String logFile;
+
+    @Option(names = { "-c", "--slingHome" }, description = "the sling context directory (default sling)", required = false)
+    private String slingHome;
+
+    //AS TODO: does this still apply here
+    @Option(names = { "-i", "--launcherHome" }, description = "the launcher home directory (default launcher)", required = false)
+    private String launcherHome;
+
+    @Option(names = { "-a", "--address" }, description = "the interface to bind to (use 0.0.0.0 for any)", required = false)
+    private String address;
+
+    @Option(names = { "-p", "--port" }, description = "the port to listen to (default 8080)", required = false)
+    private String port;
+
+    @Option(names = { "-r", "--context" }, description = "the root servlet context path for the http service (default is /)", required = false)
+    private String contextPath;
+
+    @Option(names = { "-n", "--noShutdownHook" }, description = "don't install the shutdown hook")
+    private boolean noShutdownHook;
+
+    @Option(names = { "-v", "--verbose" }, description = "the feature launcher is verbose on launch", required = false)
+    private boolean verbose;
+
+    @Option(names = {"-D", "--define"}, description = "sets property n to value v. Make sure to use this option *after* the jar filename. " +
+        "The JVM also has a -D option which has a different meaning", required = false)
+    private Map<String, String> properties = new HashMap<>();
+
+    @Option(names = { "-h", "--help" }, usageHelp = true, description = "Display the usage message.")
+    private boolean helpRequested;
+
+    // The name of the environment variable to consult to find out
+    // about sling.home
+    private static final String ENV_SLING_HOME = "SLING_HOME";
+
+//    /**
+//     * The name of the configuration property indicating the
+//     * {@link ControlAction} to be taken in the {@link #doControlAction()}
+//     * method.
+//     */
+//    protected static final String PROP_CONTROL_ACTION = "sling.control.action";
+
+    /**
+     * The name of the configuration property indicating the socket to use for
+     * the control connection. The value of this property is either just a port
+     * number (in which case the host is assumed to be <code>localhost</code>)
+     * or a host name (or IP address) and port number separated by a colon.
+     */
+    protected static final String PROP_CONTROL_SOCKET = "sling.control.socket";
+
+    /** The Sling configuration property name setting the initial log level */
+    private static final String PROP_LOG_LEVEL = "org.apache.sling.commons.log.level";
+
+    /** The Sling configuration property name setting the initial log file */
+    private static final String PROP_LOG_FILE = "org.apache.sling.commons.log.file";
+
+    /**
+     * The configuration property setting the port on which the HTTP service
+     * listens
+     */
+    private static final String PROP_PORT = "org.osgi.service.http.port";
+
+    /**
+     * The configuration property setting the context path where the HTTP service
+     * mounts itself.
+     */
+    private static final String PROP_CONTEXT_PATH = "org.apache.felix.http.context_path";
+
+    /**
+     * Host name or IP Address of the interface to listen on.
+     */
+    private static final String PROP_HOST = "org.apache.felix.http.host";
+
+    /**
+     * Name of the configuration property (or system property) indicating
+     * whether the shutdown hook should be installed or not. If this property is
+     * not set or set to {@code true} (case insensitive), the shutdown hook
+     * properly shutting down the framework is installed on startup. Otherwise,
+     * if this property is set to any value other than {@code true} (case
+     * insensitive) the shutdown hook is not installed.
+     * <p>
+     * The respective command line option is {@code -n}.
+     */
+    private static final String PROP_SHUTDOWN_HOOK = "sling.shutdown.hook";
+
+
+    @Override
+    public void run() {
+        try {
+            URL mainFeatureURL = checkFeatureFile(mainFeatureFile);
+            if(mainFeatureURL == null) {
+                mainFeatureURL = getClass().getResource("/feature-sling12.json");
+            }
+            List<String> argumentList = new ArrayList<>();
+            argumentList.add("-f");
+            argumentList.add(mainFeatureURL.toString());
+            if(additionalFeatureFile != null) {
+                for (String additional : additionalFeatureFile) {
+                    URL additionalURL = checkFeatureFile(additional);
+                    if (additionalURL != null) {
+                        argumentList.add("-f");
+                        argumentList.add(additionalURL.toString());
+                    }
+                }
+            }
+            if(StringUtils.isNotEmpty(controlAddress)) {
+                addArgument(argumentList, PROP_CONTROL_SOCKET, controlAddress);
+            }
+            if(StringUtils.isNotEmpty(logLevel)) {
+                addArgument(argumentList, PROP_LOG_LEVEL, logLevel);
+            }
+            if(StringUtils.isNotEmpty(logFile)) {
+                addArgument(argumentList, PROP_LOG_FILE, logFile);
+            }
+            if(StringUtils.isNotEmpty(port)) {
+                addArgument(argumentList, PROP_PORT, port);
+            }
+            if(StringUtils.isNotEmpty(address)) {
+                addArgument(argumentList, PROP_HOST, address);
+            }
+            if(StringUtils.isNotEmpty(contextPath)) {
+                addArgument(argumentList, PROP_CONTEXT_PATH, contextPath);
+            }
+            if(verbose) {
+                argumentList.add("-v");
+            }
+            System.out.println("Before Launching Feature Launcher, arguments: " + argumentList);
+            Main.main(argumentList.toArray(new String[]{}));
+        } catch(Throwable t) {
+            System.out.println("Caught an Exception: " + t.getLocalizedMessage());
+            t.printStackTrace();
+        }
+    }
+
+    private void addArgument(List<String> list, String key, String value) {
+        list.add("-D");
+        list.add(key + "=" + value);
+    }
+
+    private URL checkFeatureFile(String featureFile) {
+        URL answer = null;
+        if(featureFile != null && !featureFile.isEmpty()) {
+            try {
+                URL check = new URL(featureFile);
+                check.toURI();
+                answer = check;
+            } catch (MalformedURLException | URISyntaxException e) {
+                // Try it as a file
+                File check = new File(mainFeatureFile);
+                if (!check.exists() || !check.canRead()) {
+                    throw new RuntimeException("Given Feature File is not a valid URL or File: '" + mainFeatureFile + "'", e);
+                }
+                try {
+                    answer = check.toURI().toURL();
+                } catch (MalformedURLException ex) {
+                    throw new RuntimeException("Given Feature File cannot be converted to an URL: '" + mainFeatureFile + "'", e);
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        CommandLine.run(new SlingStarter(), args);
+    }
+
+}
