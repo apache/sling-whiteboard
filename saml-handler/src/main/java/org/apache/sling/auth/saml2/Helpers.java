0a21421373fcbf68ff0791bfb0f7b1b3c2ee753f
Cris Rockwell <cmrockwe@umich.edu>
2020-02-19 15:05:53 -0500

    added more static helpers

--- saml-handler/src/main/java/org/apache/sling/auth/saml2/Helpers.java
+++ saml-handler/src/main/java/org/apache/sling/auth/saml2/Helpers.java
@@ -15,20 +15,34 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
+ *
  */
 
 package org.apache.sling.auth.saml2;
 
+import net.shibboleth.utilities.java.support.security.RandomIdentifierGenerationStrategy;
+import org.opensaml.core.xml.XMLObject;
 import org.opensaml.core.xml.XMLObjectBuilderFactory;
 import org.opensaml.core.xml.config.XMLObjectProviderRegistrySupport;
+import org.opensaml.core.xml.io.Marshaller;
+import org.opensaml.core.xml.io.MarshallingException;
+import org.opensaml.saml.common.SignableSAMLObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.w3c.dom.Element;
 import javax.xml.namespace.QName;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.StringWriter;
 
 public class Helpers {
 
     private static Logger logger = LoggerFactory.getLogger(Helpers.class);
+    private static RandomIdentifierGenerationStrategy secureRandomIdGenerator;
+    static {
+        secureRandomIdGenerator = new RandomIdentifierGenerationStrategy();
+    }
 
     public static <T> T buildSAMLObject(final Class<T> clazz) {
         T object = null;
@@ -42,5 +56,41 @@ public class Helpers {
             throw new IllegalArgumentException("Could not create SAML object");
         }
         return object;
+    }
+
+    public static String generateSecureRandomId() {
+        return secureRandomIdGenerator.generateIdentifier();
+    }
+
+    public static void logSAMLObject(final XMLObject object) {
+        Element element = null;
+        if (object instanceof SignableSAMLObject && ((SignableSAMLObject)object).isSigned() && object.getDOM() != null) {
+            element = object.getDOM();
+        } else {
+            try {
+                Marshaller out = XMLObjectProviderRegistrySupport.getMarshallerFactory().getMarshaller(object);
+                out.marshall(object);
+                element = object.getDOM();
+
+            } catch (MarshallingException e) {
+                logger.error(e.getMessage(), e);
+            }
+        }
+
+        try {
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            StreamResult result = new StreamResult(new StringWriter());
+            DOMSource source = new DOMSource(element);
+
+            transformer.transform(source, result);
+            String xmlString = result.getWriter().toString();
+
+            logger.info(xmlString);
+        } catch (TransformerConfigurationException e) {
+            logger.error("TransformerConfigurationException in logSAMLObject", e);
+        } catch (TransformerException e) {
+            logger.error("TransformerException in logSAMLObject", e);
+        }
     }
 }
