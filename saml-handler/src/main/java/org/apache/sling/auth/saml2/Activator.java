08d5f0bb6c299f650a0f50c96acf8c0f60ebabdf
Cris Rockwell <cmrockwe@umich.edu>
2020-02-25 10:02:38 -0500

    Classloading pain

--- saml-handler/src/main/java/org/apache/sling/auth/saml2/Activator.java
+++ saml-handler/src/main/java/org/apache/sling/auth/saml2/Activator.java
@@ -24,6 +24,7 @@ import org.opensaml.core.config.InitializationService;
 import org.opensaml.xmlsec.config.impl.JavaCryptoValidationInitializer;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.wiring.BundleWiring;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.security.Provider;
@@ -34,9 +35,21 @@ public class Activator implements BundleActivator {
     private static final Logger logger = LoggerFactory.getLogger(BundleActivator.class);
 
     public void start(BundleContext context) throws Exception {
+        BundleWiring bundleWiring = context.getBundle().adapt(BundleWiring.class);
+        ClassLoader loader = bundleWiring.getClassLoader();
+
+        Thread thread = Thread.currentThread();
+        thread.setContextClassLoader(InitializationService.class.getClassLoader());
+        try {
+            InitializationService.initialize();
+        } finally {
+            thread.setContextClassLoader(loader);
+        }
+
+
+
         logger.info("Activating Apache Sling SAML2 SP Bundle. And Initializing JCE, Java Cryptographic Extension");
         JavaCryptoValidationInitializer jcvi = new JavaCryptoValidationInitializer();
-
         try {
             jcvi.init();
             for (Provider jceProvider : Security.getProviders()) {
@@ -48,20 +61,21 @@ public class Activator implements BundleActivator {
         }
 
 /*
-TODO: Check whether SLing Dev's have advice about this classloading / initialization
-The suggestion in this post
+TODO: What is the proper way around this classloading problem?
+One suggestion in this post
 https://medium.com/@dehami.deshan/commencing-migration-towards-the-checked-flag-opensaml-3-cc62d3faa3b0
-fixes a issue similar to what is discussed below
+
+...fixes a issue similar to what is discussed below
 https://shibboleth.1660669.n2.nabble.com/Null-returned-by-XMLObjectProviderRegistrySupport-getBuilderFactory-td7643173.html
+
+...and here
+https://stackoverflow.com/questions/37948303/opensaml3-resource-not-found-default-config-xml-in-osgi-container
+
+...and here
+https://stackoverflow.com/questions/2198928/better-handling-of-thread-context-classloader-in-osgi?noredirect=1&lq=1
+
 */
-        Thread thread = Thread.currentThread();
-        ClassLoader loader = thread.getContextClassLoader();
-        thread.setContextClassLoader(InitializationService.class.getClassLoader());
-        try {
-            InitializationService.initialize();
-        } finally {
-            thread.setContextClassLoader(loader);
-        }
+
     }
 
     public void stop(BundleContext context) throws Exception {
