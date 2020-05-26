2e6e9cbc08e18b55ca6efe89ccd3800f11c842f3
Bertrand Delacretaz <bdelacretaz@apache.org>
2020-05-26 18:33:07 +0200

    Server-side aggregation using GraphQL queries and DataFetcher

---
+++ org.apache.sling.graphql.samples.website/src/main/java/org/apache/sling/graphql/samples/website/SamplesDataFetcherProvider.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.sling.graphql.samples.website;
+
+import graphql.schema.DataFetcher;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.graphql.api.graphqljava.DataFetcherProvider;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.osgi.service.component.annotations.Component;
+
+import java.io.IOException;
+
+@Component(service = DataFetcherProvider.class, property = { "namespace=samples" })
+public class SamplesDataFetcherProvider implements DataFetcherProvider {
+
+    @Override
+    public @Nullable DataFetcher<Object> createDataFetcher(@NotNull Resource r, @NotNull String name,
+            @Nullable String options, @Nullable String source) throws IOException {
+                
+        if(SeeAlsoDataFetcher.NAME.equals(name)) {
+            return new SeeAlsoDataFetcher(r);
+        } else if(ValueMapDataFetcher.NAME.equals(name)) {
+            return new ValueMapDataFetcher(r);
+        }
+        return null;
+    }
+}
---
+++ org.apache.sling.graphql.samples.website/src/main/java/org/apache/sling/graphql/samples/website/SeeAlsoDataFetcher.java
@@ -0,0 +1,89 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.sling.graphql.samples.website;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ValueMap;
+
+import graphql.schema.DataFetcher;
+import graphql.schema.DataFetchingEnvironment;
+
+class SeeAlsoDataFetcher implements DataFetcher<Object> {
+
+    public static final String NAME = "seeAlso";
+
+    private final Resource resource;
+
+    public static class ArticleRef {
+        private final String title;
+        private final String path;
+
+        ArticleRef(Resource r) {
+            final ValueMap vm = r.adaptTo(ValueMap.class);
+            title = vm == null ? null : vm.get("title", String.class);
+            path = r.getPath();
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getPath() {
+            return path;
+        }
+    }
+
+    SeeAlsoDataFetcher(Resource r) {
+        this.resource = r;
+    }
+
+    private static ArticleRef toArticleRef(ResourceResolver resolver, String nodeName) {
+        final String jcrQuery = "/jcr:root/content/articles//" + nodeName;
+        final Iterator<Resource> it = resolver.findResources(jcrQuery, "xpath");
+        if(!it.hasNext()) {
+            throw new RuntimeException("No Resource found:" + jcrQuery);
+        }
+        final ArticleRef result = new ArticleRef(it.next());
+        if(it.hasNext()) {
+            throw new RuntimeException("More than one Resource found:" + jcrQuery);
+        }
+        return result;
+    }
+
+    @Override
+    public Object get(DataFetchingEnvironment environment) throws Exception {
+
+        // Our "see also" field only contains node names - this demonstrates
+        // using a query to get their full paths
+        // 
+        final ValueMap vm = resource.adaptTo(ValueMap.class);
+        if(vm != null) {
+            return Arrays
+                .stream(vm.get(NAME, String[].class))
+                .map(it -> toArticleRef(resource.getResourceResolver(), it))
+                .toArray();
+        }
+        return null;
+    }
+}
