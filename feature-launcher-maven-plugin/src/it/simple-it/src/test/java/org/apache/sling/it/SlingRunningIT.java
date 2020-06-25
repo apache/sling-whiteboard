eec8fcfb9e253eb9a2d7296d49fa1507be84d58c
Robert Munteanu <rombert@apache.org>
2020-06-25 13:40:23 +0200

    SLING-9526 - Allow launching feature model applications in external processes, non-blocking
    - update the IT to start Sling 12 (for now), validating the plugin
    - run the IT by default
    - remove the unit test, it did not work properly

---
+++ feature-launcher-maven-plugin/src/it/simple-it/src/test/java/org/apache/sling/it/SlingRunningIT.java
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.apache.sling.it;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import  org.apache.http.impl.client.*;
+import  org.apache.http.client.methods.*;
+
+public class SlingRunningIT {
+
+    @Test
+    public void slingIsUp() throws Exception {
+
+        int port = Integer.getInteger("SLING_HTTP_PORT", 8080);
+
+        try ( CloseableHttpClient httpclient = HttpClients.createDefault() ) {
+            HttpGet get = new HttpGet("http://localhost:" + port + "/");
+            for ( int i = 0; i < 30; i++ ) {
+                try ( CloseableHttpResponse response = httpclient.execute(get) ) {
+                    System.out.println("Status line = " + response.getStatusLine().toString());
+                    int statusCode = response.getStatusLine().getStatusCode();
+                    if ( (statusCode / 100 == 2) || (statusCode / 100 == 3) ) {
+                        System.out.println("Sling is ready");
+                        return;
+                    }
+                    Thread.sleep(1000l);
+                }
+            }
+
+            throw new RuntimeException("Sling is not yet ready, failing");
+        }
+    }
+}
