9d5d12b2a9b3193ab1f1541469e4a149fbae5709
Bertrand Delacretaz <bdelacretaz@apache.org>
2020-08-11 16:47:29 +0200

    HTTP caching experiments skeleton

---
+++ http-caching-experiments/public-html/private.lua
@@ -0,0 +1,14 @@
+require "string"
+local os = require 'os'
+
+--[[
+     This is the default method name for Lua handlers, see the optional
+     function-name in the LuaMapHandler directive to choose a different
+     entry point.
+--]]
+function handle(r)
+    r.content_type = "text/plain"
+    r:puts("Cache-Control: Private at " .. os.time() .. "\n")
+    r.headers_out["Cache-Control"] = 'Private'
+    return apache2.OK
+end
