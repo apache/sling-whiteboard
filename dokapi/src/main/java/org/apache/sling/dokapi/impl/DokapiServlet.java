c74d4f6a3709d79479fc8a4300f885ce297c7221
Bertrand Delacretaz <bdelacretaz@apache.org>
2020-08-26 16:43:46 +0200

    Basic navigation, properties filtering and api/metadata/content/children output

---
+++ dokapi/src/main/java/org/apache/sling/dokapi/impl/DokapiServlet.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.sling.dokapi.impl;
+
+import java.io.IOException;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.servlet.Servlet;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.osgi.service.component.annotations.Component;
+
+/** Main Dokapi servlet */
+@Component(service = Servlet.class,
+    name="org.apache.sling.servlets.get.DefaultGetServlet",
+    property = {
+            "service.description=Sling Dokapi Servlet",
+            "service.vendor=The Apache Software Foundation",
+
+            "sling.servlet.resourceTypes=sling/servlet/default",
+            "sling.servlet.prefix:Integer=-1",
+
+            "sling.servlet.methods=GET",
+            "sling.servlet.methods=HEAD",
+            "sling.servlet.selectors=dkp",
+            "sling.servlet.extension=json",
+    })
+public class DokapiServlet extends SlingSafeMethodsServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws IOException {
+        final PipelineContext pc = new PipelineContext(request);
+        new MetadataProcessor().process(pc);
+        new ChildrenProcessor().process(pc);
+        new ContentProcessor().process(pc);
+        final JsonObject json = pc.build();
+
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+        response.getWriter().write(json.toString());
+    }
+}
---
+++ dokapi/src/main/java/org/apache/sling/dokapi/impl/P.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.sling.dokapi.impl;
+
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+
+import org.apache.sling.api.resource.ValueMap;
+
+class P {
+    static final String [] TITLE_PROPS = { "jcr:title", "title" };
+    static final String [] NAME_PROPS = { "jcr:name", "name" };
+    static final String [] TEXT_PROPS = { "jcr:text", "text" };
+    static final String [] DESCRIPTION_PROPS = { "jcr:description", "description" };
+
+    static boolean maybeAdd(JsonObjectBuilder b, String propName, String jsonName, ValueMap vm) {
+        if(vm.containsKey(propName)) {
+            final Object value = vm.get(propName);
+            if(value != null) {
+                if(value instanceof Object[]) {
+                    final JsonArrayBuilder a = Json.createArrayBuilder();
+                    for(Object o : (Object[])value) {
+                        a.add(o.toString());
+                    }
+                    b.add(jsonName, a.build());
+                } else {
+                    b.add(jsonName, value.toString());
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    static void maybeAddOneOf(JsonObjectBuilder b, String propName, ValueMap vm, String [] props) {
+        for(String prop : NAME_PROPS) {
+            if(maybeAdd(b, prop, propName, vm)) {
+                break;
+            }
+        }
+    }
+
+    static boolean ignoreProperty(String key) {
+        return key.startsWith("jcr:");
+    }
+
+    static boolean ignoreResource(String name) {
+        return name.startsWith("rep:");
+    }
+
+    static String convertName(String in) {
+        return in.replace("sling:", "_");
+    }
+
+    static boolean isMetadata(String propName) {
+        return propName.startsWith("sling:");
+    }
+}
