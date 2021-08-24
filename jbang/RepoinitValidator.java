0326b32f86a99d91737fae32c474d80eaf15620f
Bertrand Delacretaz <bdelacretaz@apache.org>
2021-08-24 10:58:27 +0200

    Add usage info

--- jbang/RepoinitValidator.java
+++ jbang/RepoinitValidator.java
@@ -6,6 +6,12 @@ import java.io.Reader;
 import java.io.InputStreamReader;
 import java.util.List;
 
+/** To call this directly from GitHub with https://www.jbang.dev/ :
+ *
+ *      jbang trust add https://github.com/apache/sling-whiteboard/
+ *      jbang https://github.com/apache/sling-whiteboard/blob/master/jbang/RepoinitValidator.java < some-repoinit-script.txt
+ *
+ */
 class RepoinitValidator {
 
     public static void main(String[] args) {
