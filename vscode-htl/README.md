79c3311447665d2bf5b21f3b996ee282e9aebb3a
Robert Munteanu <rombert@apache.org>
2021-12-09 13:47:44 +0100

    Cleanups: remove sample actions, rename to vscode-htl

--- vscode-htl/README.md
+++ vscode-htl/README.md
@@ -1,7 +1,3 @@
 # Visual Studio code extension for HTL
 
-TODO:
-- provide completions based on default script bindings
-- provide completions based on known types of script bindings (SlingHttpServletRequest, Resource)
-- provide completions for objects inferred from data-sly-use.$IDENTIFIER
-- provide completions for objects provided by data-sly-repeat and friends (first/last/etc)
+Provides basic auto-completions.
--- vscode-htl/src/extension.ts
+++ vscode-htl/src/extension.ts
@@ -6,27 +6,9 @@ import { HtlCompletionItemProvider } from './htlCompletionItemProvider';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-hello" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let helloWorld = vscode.commands.registerCommand('vscode-hello.helloWorld', () => {
-		vscode.window.showErrorMessage('Hello VS Code');
-	});
-
-	let now = vscode.commands.registerCommand('vscode-hello.now', () => {
-		vscode.window.showWarningMessage(new Date().toISOString());
-	});
-
-	context.subscriptions.push(helloWorld, now);
-
 	let completionsPath = vscode.Uri.joinPath(context.extensionUri, "data");
-
-	vscode.languages.registerCompletionItemProvider('html', new HtlCompletionItemProvider(completionsPath));
+	let disposable = vscode.languages.registerCompletionItemProvider('html', new HtlCompletionItemProvider(completionsPath));
+	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
