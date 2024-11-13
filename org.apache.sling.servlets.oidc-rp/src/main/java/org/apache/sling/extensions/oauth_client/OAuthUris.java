4c8e40c1790eceb66f78a649525f88111a146d5d
Robert Munteanu <rombert@apache.org>
2024-11-14 00:15:38 +0100

    chore(oauth-client): no reason for OAuthUri to throw OAuthException

--- org.apache.sling.servlets.oidc-rp/src/main/java/org/apache/sling/extensions/oauth_client/OAuthUris.java
+++ org.apache.sling.servlets.oidc-rp/src/main/java/org/apache/sling/extensions/oauth_client/OAuthUris.java
@@ -34,9 +34,8 @@ public abstract class OAuthUris {
      * @param request The current request
      * @param redirectPath The local redirect path to use after completing the OIDC flow
      * @return a local URI
-     * @throws OAuthException in case anything goes wrong
      */
-    public static URI getOidcEntryPointUri(ClientConnection connection, SlingHttpServletRequest request, String redirectPath) throws OAuthException {
+    public static URI getOidcEntryPointUri(ClientConnection connection, SlingHttpServletRequest request, String redirectPath) {
         StringBuilder uri = new StringBuilder();
         uri.append(request.getScheme()).append("://").append(request.getServerName());
         boolean needsExplicitPort = ( "https".equals(request.getScheme()) && request.getServerPort() != 443 )
